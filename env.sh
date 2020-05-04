c4ca14fbe8efe6733e9ff16159bf6029ad768236
david.lo <rurikaxx@gmail.com>
2020-05-04 15:12:49 +0800

    for qnap

--- env.sh
+++ env.sh
@@ -41,6 +41,8 @@ do
         1)
             # 啟動 php
             docker-compose up -d --build php
+            # 啟動 php-7.3
+            docker-compose up -d --build php-7.3
             # 啟動 nginx
             docker-compose up -d --build nginx
             # 啟動 mysql
